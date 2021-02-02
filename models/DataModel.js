fadbd077ae787f09486ce39c68bd95958be47c09
Konstantin <konstantin.ay95@gmail.com>
2021-02-02 19:39:50 +0100

    Added schema to DataModel

--- models/DataModel.js
+++ models/DataModel.js
@@ -5,6 +5,41 @@ class DataModel {
     this.extension = extension;
     this.duration = duration;
   }
+
+  static getSchema(objectName) {
+    return {
+      [`${objectName}.*.name`]: {
+        isString: {
+          errorMessage: "File name has to be a string.",
+        },
+        notEmpty: {
+          errorMessage: "File name cannot be empty.",
+        },
+      },
+      [`${objectName}.*.data`]: {
+        notEmpty: {
+          errorMessage: "File has to have data.",
+        },
+      },
+      [`${objectName}.*.extension`]: {
+        isString: {
+          errorMessage: "Extension has to be a string.",
+        },
+        notEmpty: {
+          errorMessage: "Extension cannot be empty.",
+        },
+      },
+      [`${objectName}.*.duration`]: {
+        optional: { options: { nullable: true } },
+        isInt: {
+          errorMessage: "Duration has to be a positive number.",
+          options: {
+            min: 0,
+          },
+        },
+      },
+    };
+  }
 }
 
 module.exports = { DataModel };
